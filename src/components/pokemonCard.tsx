efa0c258c523bd869054b4eeeca19d6950ee6589
ANTOINE <ANT.FOUB@GMAIL.COM>
2024-10-22 17:04:36 +0200

    conditional rendering

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,10 +1,27 @@
 function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[0];
+
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokémon" />
-      <figcaption> Bulbasaur </figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt="Pokemon" />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption> {pokemon.name} </figcaption>
     </figure>
   );
 }
 
-export default PokemonCard
+export default PokemonCard;
